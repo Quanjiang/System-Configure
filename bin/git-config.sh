34587d11e02006c50f3d4af24288faa63f290ea0
quanjiang <quanjiang.gu@foxmail.com>
2014-04-07 15:17:37 +0000

    a

---
+++ bin/git-config.sh
@@ -0,0 +1,6 @@
+git config --global push.default simple
+git config --global user.name "quanjiang"
+git config --global user.email "quanjiang.gu@foxmail.com"
+git config --global core.editor emacs
+git config --global color.ui auto
+
